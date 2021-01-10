823d9d6b87eeb972ec83fb41680061d570763ff5
Eryk Jalowiec <eryk.jalowiec@gmail.com>
2021-01-10 22:06:09 +0100

    added nonlinearFunction and derivative of this function

--- nonlinear-equations/nonlinear-equations.cpp
+++ nonlinear-equations/nonlinear-equations.cpp
@@ -1,8 +1,27 @@
-﻿//
+﻿// https://github.com/ejalowiec/nm-nonlinear-equations
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+// define your function and its derivative as a fixed value at the bottom of the file
+double nonlinearFunction(double);
+double derivativeOfFunction(double);
+
 int main() {
-    cout << "Hello World!\n";
+
+	cout << "Function: x^3 - 1" << endl;
+	cout << "Derivative: 3x^2" << endl;
+
+
+
+
+}
+
+double nonlinearFunction(double x) {
+	return pow(x, 3) - 1;
+}
+
+double derivativeOfFunction(double x) {
+	return pow(x, 2);
 }
