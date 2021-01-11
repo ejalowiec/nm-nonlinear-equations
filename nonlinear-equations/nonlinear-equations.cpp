547ae05103e0865c17065babf07f7336ef2e162e
Eryk Jalowiec <eryk.jalowiec@gmail.com>
2021-01-11 12:31:24 +0100

    modified the newtonMethod function to be compliant with ex2

--- nonlinear-equations/nonlinear-equations.cpp
+++ nonlinear-equations/nonlinear-equations.cpp
@@ -8,19 +8,19 @@ using namespace std;
 // define your function and its derivative as a fixed value at the bottom of the file
 double nonlinearFunction(double);
 double derivativeOfFunction(double);
-double newtonMethod(double, double, double);
+double newtonMethod_ex2(double, double);
 
 int main() {
 	// set the interval
 	double a = 0.01, b = 2;
-	double errorValue = 1e-4;
+	// double precision = 1e-4;
 
 	cout << "Function: 1/3x^3 - 1" << endl;
 	cout << "Derivative: x^2" << endl;
-	cout << "Interval: a=0.01, b=2" << endl;
-	cout << "f(" << a << ")=" << nonlinearFunction(a) << setw(8) << "f(" << b << ")=" << nonlinearFunction(b) << endl << endl;
+	cout << "Interval: a = 0.01, b = 2" << endl;
+	cout << "f(" << a << ") = " << nonlinearFunction(a) << setw(8) << "f(" << b << ") = " << nonlinearFunction(b) << endl << endl;
 	cout << "Newton's method:\n\n";
-	cout << "\nF(x)=0 <=> x=" << newtonMethod(a, b, errorValue) << endl;
+	cout << "\nF(x) = 0 <=> x = " << newtonMethod_ex2(a, b) << endl;
 
 	cout << endl;
 	return 0;
@@ -34,10 +34,12 @@ double derivativeOfFunction(double x) {
 	return pow(x, 2);
 }
 
-double newtonMethod(double a, double b, double error) {
+double newtonMethod_ex2(double a, double b) {
 	if (nonlinearFunction(a) * nonlinearFunction(b) < 0 and a < b) {
 		unsigned int counter = 0;
-		while (abs(nonlinearFunction(a)) > error) {
+		double precision = NULL;
+		cout << "Specify the precision: "; cin >> precision; cout << endl;
+		while (abs(nonlinearFunction(a)) > precision) {
 			a -= nonlinearFunction(a) / derivativeOfFunction(a);
 			cout << "Iteration " << counter << ": " << setprecision(9) << a << endl;
 			counter++;
